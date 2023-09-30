cf8250b503ef6d1e447b919da8406c7725cf3147
Jose Sanchez <joseangeldejesussanchezflores@gmail.com>
2023-09-29 21:14:49 -0600

    JADJSD 29-09-2023 Creacion del componente potencias y el uso del Formsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
 import { AppComponent } from './app.component';
+import { PotenciasComponent } from './Operaciones/potencias/potencias.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CinepolisComponent } from './casos/cinepolis/cinepolis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PotenciasComponent,
+    CinepolisComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
